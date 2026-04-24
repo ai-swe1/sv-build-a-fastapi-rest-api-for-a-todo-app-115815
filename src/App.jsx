f1903da8531f0e0a1ed0869b084fcd48ae66d358
Sajo Sam <66501284+sajosam@users.noreply.github.com>
2026-04-24 11:59:30 +0530

    feat: add src/App.jsx [15/26]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4 mt-4">
+      <h1 className="text-3xl font-bold">Vite + Tailwind + React</h1>
+    </div>
+  );
+}
+
+export default App;
